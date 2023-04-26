6e6f9958adbf9146ac32c53ed504e09502600d41
dan1c1m0 <lyashenko_danya00@mail.ru>
2023-04-26 23:16:47 +0600

    new cycles

--- 013 script.js
+++ 013 script.js
@@ -28,12 +28,12 @@ for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
 
-    if (a == null || b == null || a == '' || b == '' || a.length > 50) {
-        console.log('error');
-        i--;
-    } else {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB[a] = b;
         console.log('done');
+    } else {
+        console.log('error');
+        i--;
     }
 }
 
